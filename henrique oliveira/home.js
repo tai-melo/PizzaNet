e2eb70afdfc9d6b6b20fe953c9f7ffa5d69a6519
nomegustaa <gustavocarvalhodasilva015@gmail.com>
2023-02-01 22:16:05 -0300

    backEnd na tela de home e ícone do modal

---
+++ henrique oliveira/home.js
@@ -0,0 +1,55 @@
+// função para o usuário pode sair da conta dele
+const buttonSair = document.querySelector('#buttonSair')
+
+buttonSair.addEventListener('click', logout)
+
+
+
+
+
+firebase.auth().onAuthStateChanged(user => {
+    if(user){
+        console.log('usuário existe', user)
+        usersInfo(user)
+        const disableLogin = document.querySelector('.activeLogin')
+        const disableCadastro = document.querySelector('.activeCadastro')
+        disableLogin.classList.add('desativado')
+        disableCadastro.classList.add('desativado')
+    }else{
+        buttonSair.classList.add('desativado')
+    }
+})
+
+function logout(){
+    firebase.auth().signOut().then( () => {
+        window.location.href = '../Ghabriel/login/index.html';
+        console.log('deu certo')
+    }).catch( () =>{
+        alert('Erro ao sair')
+    })
+}
+
+function usersInfo(user){
+    firebase.firestore()
+        .collection('infoUsers')
+        .where('user.uid', '==', user.uid)
+        .get()
+        .then(snapshot => {
+            const inforUsers = snapshot.docs.map(doc => doc.data())
+            getNameUser(inforUsers)
+        })
+}
+
+
+function getNameUser(inforUsers){
+    inforUsers.forEach(dadosUsuario => {
+
+        const userName = document.querySelector('.userName')
+
+        userName.innerHTML = 'Olá, ' + dadosUsuario.nome
+        console.log(dadosUsuario.nome)
+        console.log(inforUsers)
+    });
+}
+
+
