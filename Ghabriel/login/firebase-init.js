455ae18508dc81f3480e385bb59e83552e21e8d0
nomegustaa <gustavocarvalhodasilva015@gmail.com>
2023-02-01 22:14:42 -0300

    ScriptFireBase

---
+++ Ghabriel/login/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1sYkRIV8Z40tAEiAX7PQqRnCFVrUWcWc",
+    authDomain: "usuarios-pizzaria.firebaseapp.com",
+    projectId: "usuarios-pizzaria",
+    storageBucket: "usuarios-pizzaria.appspot.com",
+    messagingSenderId: "551622724614",
+    appId: "1:551622724614:web:20533027fd4c087f5ac787"
+};
+firebase.initializeApp(firebaseConfig);
